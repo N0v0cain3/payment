b3ef8cf4028c9b497c72cad528078ec072c47392
Shivam Mehta <shivam.mehta2019@vitstudent.ac.in>
2020-07-29 13:11:10 +0530

    subs

--- app.js
+++ app.js
@@ -56,7 +56,50 @@ app.post('/payment', function (req, res) {
         });
 })
 
+async function lol() {
+    const product = await stripe.products.create({
+        name: 'Rajat Sablok',
+        description: 'rajat bhaiya pro'
+    });
+    console.log(product)
+
+    const price = await stripe.prices.create({
+        product: 'prod_HjcakjZgZl2mdH',
+        unit_amount: 1000,
+        currency: 'inr',
+        recurring: {
+            interval: 'month',
+        },
+    });
+    console.log(price)
+
+    let session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [{
+            price: 'price_1HA9NqD3a83c3w5dGjznxc9B',
+            quantity: 1,
+        }],
+        mode: 'subscription',
+        success_url: 'https://example.com/success?session_id=cs_test_bbJRHqDpyjcWhGnXM7K1hx9Af3oyxH2Jo8Cl3DE7YzIqtph67A2VRjoe',
+        cancel_url: 'https://example.com/cancel',
+    });
+    session.success_url = `https://example.com/success?session_id=${session.id}`
+
+    console.log(session)
+
+
+
+
+
+}
+
+
+
+
+
 app.listen(port, function (error) {
     if (error) throw error
     console.log("Server created Successfully")
+    lol()
+
 }) 
