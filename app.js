e617487e5dee0ac594dd38f63c0d03a3441f249e
Shivam Mehta <shivam.mehta2019@vitstudent.ac.in>
2020-08-01 23:35:51 +0530

    ez

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const Razorpay = require('razorpay')
+const fetch = require('node-fetch')
 require('dotenv').config()
 var instance = new Razorpay({
     key_id: process.env.KEY_ID,
     key_secret: process.env.KEY_SECRET
 })
-console.log(instance)
+var z = instance.plans.all()
+console.log(z)
 const app = express();
 const port = 3000
+
+let base64 = require('base-64');
+
+let url = 'https://api.razorpay.com/v1/plans';
+
+// let headers = new Headers();
+
+//headers.append('Content-Type', 'text/json');
+// let headers = {}
+fetch(url, {
+    method: 'GET',
+    headers: {
+        "Content-Type": "application/json",
+        'Authorization': 'Basic ' + base64.encode(process.env.KEY_ID + ":" + process.env.KEY_SECRET)
+    },
+    //credentials: 'user:passwd'
+})
+    .then(response => response.json())
+    .then(json => console.log(json.items[0].item));
+//.done();
+
+function parseJSON(response) {
+    return response.json()
+}
 
 app.listen(port, function (error) {
     if (error) throw error
